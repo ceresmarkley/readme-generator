25911a14c7522e0daa93ac1eca2f17dde81ad151
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-07-03 19:08:13 -0400

    updated const init function and changed prompt user function to const questions. This corrected bug which was preventing node index.js from running proper question sequence to user.

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
 const inquirer = require('inquirer');
-
 const { writeFile } = require('fs').promises;
+const generateMarkdown = require('./utils/generateMarkdown');
 
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+            name: 'title',
+            message: 'What is your applications title?',
         },
         {
             type: 'input',
-            name: 'location',
-            message: 'Where you from?',
+            name: 'description',
+            message: 'Please Enter a brief description of your application.',
         },
         {
             type: 'input',
-            name: 'hobby',
-            message: 'What you like to do?',
-        },{
-            type: 'input',
-            name: 'github',
-            message: 'What is your github?',
-        },{
-            type: 'input',
-            name: 'linkedIn',
-            message: 'What is your linkedin?',
+            name: 'installation',
+            message: 'How do you you install your application? ',
         },
-    ]);
-};
-
-function generateReadme(answers) {
-
-}
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you USE your application? ',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Any Contribution Guidelines to Your Cool App? ',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Application test URL link',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please Select which license you will be using. ',
+            choices: ['Apache License, Version 2.0',
+                'Common Development and Distribution License 1.0',
+                'Eclipse Public License version 2.0',
+                'GNU General Public License version 2',
+                'GNU General Public License version 3',
+                'GNU Lesser General Public License version 2.1',
+                'GNU Lesser General Public License version 3',
+                'GNU Library General Public License version 2',
+                'Mozilla Public License 2.0',
+                'The 2-Clause BSD License',
+                'The 3-Clause BSD License',
+                'The MIT License']
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter Your GitHub Username: ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Your Email: ',
+        },
+    ];
 
 
 const init = () => {
-    promptUser()
-        .then((answers) => writeFile('README.md', generateReadme(answers)))
+    inquirer.prompt(questions)
+        .then((answers) => writeFile('README.md', generateMarkdown(answers)))
         .catch((err) => console.log(err));
  };
 
